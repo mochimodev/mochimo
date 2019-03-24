982246906d7cbb3d31d8c2126675a1bf0c94d2b7
Mochimo Development Team <support@mochimo.org>
2019-03-24 14:09:48 -0400

    Updates to the Carousel/LastDay/Renew functions.

--- src/renew.c
+++ src/renew.c
@@ -11,49 +11,52 @@
 #define CAROUSEL(bnum) (get32(bnum) == Lastday)
 
 
-/* Returns VERROR on I/O errors, else VEOK. */
+/* Returns 0 on success, else error code. */
 int renew(void)
 {
    FILE *fp, *fpout;
    LENTRY le;
-   int status;
+   int message = 0;
    word32 n, m;
    static word32 sanctuary[2];
 
-   if(Sanctuary == 0) return VEOK;
-   if(Trace) plog("renew()");
+   if(Sanctuary == 0) return 0;  /* success */
+   le_close();  /* make sure ledger.dat is closed */
+   plog("Lastday 0x%0x.  Carousel begins...", Lastday);
    n = m = 0;
+   fp = fpout = NULL;
    sanctuary[0] = Sanctuary;
 
    fp = fopen("ledger.dat", "rb");
-   if(fp == NULL) return VERROR;
+   if(fp == NULL) BAIL(1);
    fpout = fopen("ledger.tmp", "wb");
-   if(fpout == NULL) { fclose(fp);  return VERROR; }
-   status = VEOK;
+   if(fpout == NULL) BAIL(2);
    for(;;) {
       if(fread(&le, sizeof(le), 1, fp) != 1) break;
       n++;
       if(sub64(le.balance, sanctuary, le.balance)) continue;
       if(cmp64(le.balance, Mfee) <= 0) continue;
-      if(fwrite(&le, sizeof(le), 1, fpout) != 1) {
-         status = VERROR;
-         break;
-      }
+      if(fwrite(&le, sizeof(le), 1, fpout) != 1) BAIL(3);
       m++;
    }
    fclose(fp);
    fclose(fpout);
-   if(status == VEOK) {
-      unlink("ledger.dat");
-      if(rename("ledger.tmp", "ledger.dat")) status = VERROR;
-   }
-   if(Trace) plog("renewed %u out of %u  status: %d", n - m, n, status);
-   return status;
-}  /* end renew(); */
+   fp = fpout = NULL;
+   unlink("ledger.dat");
+   if(rename("ledger.tmp", "ledger.dat")) BAIL(4);
+   plog("%u citizens renewed out of %u", n - m, n);
+   return 0;  /* success */
+bail:
+   if(fp != NULL) fclose(fp);
+   if(fpout != NULL) fclose(fpout);
+   error("Carousel renewal code: %d (%u,%u)", message, n - m, n);
+   return message;
+}  /* end renew() */
 
 
 /* Refresh the ip list and catch up if needed.
- * Called from server()
+ * Called from server().
+ * Returns result code.
  */
 int refresh_ipl(void)
 {
@@ -83,4 +86,3 @@ bail:
    if(Trace) plog("refresh_ipl(): %d", message);
    return message;
 }  /* end refresh_ipl() */
-
