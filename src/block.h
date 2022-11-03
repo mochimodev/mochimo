1cd5b98d6621438d25c466cb01163fbf6aac6c54
Chrisdigity <chrisdigity@gmail.com>
2022-11-03 12:44:11 +1000

    update block compilation unit header to reflect changes

--- src/block.h
+++ src/block.h
@@ -1,29 +1,43 @@
 /**
- * @file bval.h
- * @brief Mochimo block validation support.
+ * @file block.h
+ * @brief Mochimo blockchain generation and validation support.
  * @copyright Adequate Systems LLC, 2018-2022. All Rights Reserved.
  * <br />For license information, please refer to ../LICENSE.md
 */
 
 /* include guard */
-#ifndef MOCHIMO_BVAL_H
-#define MOCHIMO_BVAL_H
+#ifndef MOCHIMO_BLOCK_H
+#define MOCHIMO_BLOCK_H
 
 
 /* internal support */
 #include "types.h"
+
+/* global variables */
+extern char *Maddr_opt;
 
 /* C/C++ compatible function prototypes */
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int p_val(char *fname);
-int ng_val(char *fname, word8 *bnum);
-int b_val(char *fname);
+/* block_legacy.c */
+int blockw_val_fp(FILE *fp, BTRAILER *btp);
+int blockw_val(char *fname, char *tfname);
+int blockw(char *fname, TXW *txw_clean, size_t count, char *tfname);
+int neogenw_val_fp(FILE *fp, char *tfname);
+int neogenw_val(char *fname, char *tfname);
+int neogen(char *fname, char *lfname, char *tfname);
 
+/* block.c */
+int pseudo_val_fp(FILE *fp, BTRAILER *prev_btp);
+int pseudo_val(char *pfname, char *tfname);
+int pseudo(char *fname, char *tfname);
+int validate_block(char *bcfname, char *tfname);
+
+/* end extern "C" {} for C++ */
 #ifdef __cplusplus
-}  /* end extern "C" */
+}
 #endif
 
 /* end include guard */
