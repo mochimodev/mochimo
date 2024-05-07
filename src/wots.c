62ed8e9bd896417494ece9faad824b9106e2e5f1
Mochimo Development Team <support@mochimo.org>
2024-05-07 10:24:33 +1000

    improvements to Mochimo WOTS+ implementation
    XMSS related function extensibility and most constants have been inlined
    for a Mochimo specific implementation of the WOTS+ algorithm. These
    improvements were intended to make the WOTS+ implementation a little
    easier to port to other languages for the Mochimo project, but have also
    improvement performance by almost 10%.

--- src/wots.c
+++ src/wots.c
@@ -16,106 +16,41 @@
 
 /**
  * @private
- * Hash padding parameters
-*/
-#define XMSS_HASH_PADDING_F   0
-#define XMSS_HASH_PADDING_PRF 3
-
-/**
- * @private
- * Core hashing function - sha256
-*/
-#define core_hash(out, in, inlen) sha256(in, inlen, out)
-
-/* These functions are used for OTS addresses. */
-
-static void set_key_and_mask(word32 addr[8], word32 key_and_mask)
-{
-    addr[7] = key_and_mask;
-}
-
-static void set_chain_addr(word32 addr[8], word32 chain)
-{
-    addr[5] = chain;
-}
-
-static void set_hash_addr(word32 addr[8], word32 hash)
-{
-    addr[6] = hash;
-}
-
-/**
- * @private
- * Converts the value of 'in' to 'outlen' bytes in big-endian byte order.
+ * Computes PRF(key, in), for a key of 32 bytes, and a 32-byte input.
  */
-static void ull_to_bytes(word8 *out, unsigned int outlen, unsigned long in)
+static void prf(word8 *out, const word8 in[32], const word8 *key)
 {
-    int i;
+    word8 buf2[96] = {0};
 
-    /* Iterate over out in decreasing order, for big-endianness. */
-    for (i = outlen - 1; i >= 0; i--) {
-        out[i] = in & 0xff;
-        in = in >> 8;
-    }
-}  /* end ull_to_bytes() */
-
-/**
- * @private
- * Converts @a addr to bytes using ull_to_bytes().
- */
-static void addr_to_bytes(word8 *bytes, const word32 addr[8])
-{
-    int i;
-    for (i = 0; i < 8; i++) {
-        ull_to_bytes(bytes + i*4, 4, addr[i]);
-    }
-}  /* end addr_to_bytes() */
-
-/**
- * @private
- * Computes PRF(key, in), for a key of PARAMSN bytes, and a 32-byte input.
- */
-static int prf(word8 *out, const word8 in[32], const word8 *key)
-{
-    word8 buf[2 * PARAMSN + 32];
-
-    ull_to_bytes(buf, PARAMSN, XMSS_HASH_PADDING_PRF);
-    memcpy(buf + PARAMSN, key, PARAMSN);
-    memcpy(buf + (2*PARAMSN), in, 32);
-    core_hash(out, buf, (2*PARAMSN) + 32);
-    return 0;
+    buf2[31] = 3;
+    memcpy(buf2 + 32, key, 32);
+    memcpy(buf2 + 64, in, 32);
+    sha256(buf2, 96, out);
 }  /* end prf() */
 
 /**
  * @private
  * We assume the left half is in in[0]...in[n-1]
  */
-static void thash_f(word8 *out, const word8 *in, const word8 *pub_seed,
-                    word32 addr[8])
-{
-    word8 buf[3 * PARAMSN];
-    word8 bitmask[PARAMSN];
-    word8 addr_as_bytes[32];
-    unsigned int i;
+static void thash_f(word8 *out, const word8 *in, const word8 *pub_seed, word32 addr[8]) {
+    word8 buf[96] = {0}, bitmask[32], addr_as_bytes[32];
+    unsigned long to_bytes;
 
-    /* Set the function padding. */
-    ull_to_bytes(buf, PARAMSN, XMSS_HASH_PADDING_F);
-
-    /* Generate the n-byte key. */
-    set_key_and_mask(addr, 0);
-    addr_to_bytes(addr_as_bytes, addr);
-    prf(buf + PARAMSN, addr_as_bytes, pub_seed);
-
-    /* Generate the n-byte mask. */
-    set_key_and_mask(addr, 1);
-    addr_to_bytes(addr_as_bytes, addr);
-    prf(bitmask, addr_as_bytes, pub_seed);
-
-    for (i = 0; i < PARAMSN; i++) {
-        buf[2*PARAMSN + i] = in[i] ^ bitmask[i];
+    for (int i = 0; i < 2; i++) {
+        addr[7] = i; /* from spec "set_key_and_mask" */
+        for (int j = 0; j < 8; j++) { /* convert addr longs to bytes */
+            to_bytes = addr[j];
+            for (int k = 3; k >= 0; k--) { /* as big-endian bytes */
+                addr_as_bytes[j * 4 + k] = to_bytes & 0xff;
+                to_bytes >>= 8;
+            }
+        }
+        if (i == 0) prf(buf + 32, addr_as_bytes, pub_seed);
+        else prf(bitmask, addr_as_bytes, pub_seed);
     }
-    core_hash(out, buf, 3 * PARAMSN);
-}  /* end thash_f() */
+    for (int i = 0; i < 32; i++) buf[64 + i] = in[i] ^ bitmask[i];
+    sha256(buf, 96, out);
+}
 
 /**
  * @private
@@ -124,12 +59,16 @@ static void thash_f(word8 *out, const word8 *in, const word8 *pub_seed,
  */
 static void expand_seed(word8 *outseeds, const word8 *inseed)
 {
-    word32 i;
+    word32 k;
     word8 ctr[32];
 
-    for (i = 0; i < WOTSLEN; i++) {
-        ull_to_bytes(ctr, 32, i);
-        prf(outseeds + i*PARAMSN, ctr, inseed);
+    for (int i = 0; i < 67; i++) {
+        k = i;
+        for (int j = 31; j >= 0; j--) { /* convert i to bytes */
+            ctr[j] = k & 0xff; /* fill ctr with i bytes in BE order */
+            k = k >> 8;
+        }
+        prf(outseeds + i*32, ctr, inseed);
     }
 }  /* end expand_seed() */
 
@@ -145,74 +84,43 @@ static void gen_chain(word8 *out, const word8 *in,
                       unsigned int start, unsigned int steps,
                       const word8 *pub_seed, word32 addr[8])
 {
-    word32 i;
-
     /* Initialize out with the value at position 'start'. */
-    memcpy(out, in, PARAMSN);
+    memcpy(out, in, 32);
 
     /* Iterate 'steps' calls to the hash function. */
-    for (i = start; i < (start+steps) && i < WOTSW; i++) {
-        set_hash_addr(addr, i);
+    for (word32 i = start; i < (start+steps) && i < 16; i++) {
+        addr[6] = i; /* from spec: set_hash_addr */
         thash_f(out, out, pub_seed, addr);
     }
 }  /* end gen_chain() */
 
 /**
  * @private
- * base_w algorithm as described in draft.
- * Interprets an array of bytes as integers in base w.
- * This only works when log_w is a divisor of 8.
+ * Split each byte from the input array into two 4-bit segments, storing the 
+ * higher 4 bits first in the output array (little endian order). 
  */
-static void base_w(int *output, const int out_len, const word8 *input)
-{
+static void base_w(int *output, const int out_len, const word8 *input) {
     int in = 0;
-    int out = 0;
-    word8 total;
-    int bits = 0;
-    int consumed;
-
-    for (consumed = 0; consumed < out_len; consumed++) {
-        if (bits == 0) {
-            total = input[in];
-            in++;
-            bits += 8;
-        }
-        bits -= WOTSLOGW;
-        output[out] = (total >> bits) & (WOTSW - 1);
-        out++;
+    for (int i = 0; i < out_len; i++) {
+        if (i % 2 == 0) output[i] = (input[in] >> 4) % 16; /* evens */
+        else output[i] = input[in++] & 15; /* increment on odds */
     }
-}  /* end base_w() */
+}
 
 /**
  * @private
  * Computes the WOTS+ checksum over a message (in base_w).
 */
-static void wots_checksum(int *csum_base_w, const int *msg_base_w)
-{
+static void wots_checksum(int *csum_base_w, const int *msg_base_w) {
     int csum = 0;
-    word8 csum_bytes[(WOTSLEN2 * WOTSLOGW + 7) / 8];
-    unsigned int i;
+    word8 csum_bytes[2];
 
-    /* Compute checksum. */
-    for (i = 0; i < WOTSLEN1; i++) {
-        csum += WOTSW - 1 - msg_base_w[i];
-    }
+    for (int i = 0; i < 64; i++) csum += 15 - msg_base_w[i];
 
-    /* Convert checksum to base_w. */
-    /* Make sure expected empty zero bits are the least significant bits. */
-    csum = csum << (8 - ((WOTSLEN2 * WOTSLOGW) % 8));
-    ull_to_bytes(csum_bytes, sizeof(csum_bytes), csum);
-    base_w(csum_base_w, WOTSLEN2, csum_bytes);
-}  /* end wots_checksum() */
-
-/**
- * @private
- * Takes a message and derives the matching chain lengths.
-*/
-static void chain_lengths(int *lengths, const word8 *msg)
-{
-    base_w(lengths, WOTSLEN1, msg);
-    wots_checksum(lengths + WOTSLEN1, lengths);
+    csum <<= 4;
+    csum_bytes[1] = csum & 0xff;
+    csum_bytes[0] = (csum >> 8) & 0xff;
+    base_w(csum_base_w, 3, csum_bytes);
 }
 
 /**
@@ -229,15 +137,12 @@ static void chain_lengths(int *lengths, const word8 *msg)
 void wots_pkgen(word8 *pk, const word8 *seed,
                 const word8 *pub_seed, word32 addr[8])
 {
-    word32 i;
-
     /* The WOTS+ private key is derived from the seed. */
     expand_seed(pk, seed);
 
-    for (i = 0; i < WOTSLEN; i++) {
-        set_chain_addr(addr, i);
-        gen_chain(pk + i * PARAMSN, pk + i * PARAMSN,
-                  0, WOTSW - 1, pub_seed, addr);
+    for (word32 i = 0; i < 67; i++) {
+        addr[5] = i; /* from spec: set_chain_addr */
+        gen_chain(pk + i * 32, pk + i * 32, 0, 15, pub_seed, addr);
     }
 }  /* end wots_pkgen() */
 
@@ -257,17 +162,16 @@ void wots_sign(word8 *sig, const word8 *msg,
                word32 addr[8])
 {
     int lengths[WOTSLEN];
-    word32 i;
 
-    chain_lengths(lengths, msg);
+    base_w(lengths, 64, msg);
+    wots_checksum(lengths + 64, lengths);
 
     /* The WOTS+ private key is derived from the seed. */
     expand_seed(sig, seed);
 
-    for (i = 0; i < WOTSLEN; i++) {
-        set_chain_addr(addr, i);
-        gen_chain(sig + i * PARAMSN, sig + i * PARAMSN,
-                  0, lengths[i], pub_seed, addr);
+    for (word32 i = 0; i < 67; i++) {
+        addr[5] = i; /* from spec: set_chain_addr */
+        gen_chain(sig + i * 32, sig + i * 32, 0, lengths[i], pub_seed, addr);
     }
 }  /* end wots_sign() */
 
@@ -286,14 +190,13 @@ void wots_pk_from_sig(word8 *pk,
                       const word8 *pub_seed, word32 addr[8])
 {
     int lengths[WOTSLEN];
-    word32 i;
 
-    chain_lengths(lengths, msg);
+    base_w(lengths, 64, msg);
+    wots_checksum(lengths + 64, lengths);
 
-    for (i = 0; i < WOTSLEN; i++) {
-        set_chain_addr(addr, i);
-        gen_chain(pk + i * PARAMSN, sig + i * PARAMSN,
-                  lengths[i], WOTSW - 1 - lengths[i], pub_seed, addr);
+    for (word32 i = 0; i < 67; i++) {
+        addr[5] = i; /* from spec: set_chain_addr */
+        gen_chain(pk + i * 32, sig + i * 32, lengths[i], 15 - lengths[i], pub_seed, addr);
     }
 }  /* end wots_pk_from_sig() */
 
